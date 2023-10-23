2405cce085c286976fbed25919c386f2d127f870
Francisco Saroldi-Gonzalez <francisco.saroldi-gonzalez@mrjaffesclass.com>
2023-10-23 11:32:39 -0700

    done

--- FizzBuzz/FizzBuzz.java
+++ FizzBuzz/FizzBuzz.java
@@ -1,12 +1,22 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        for(int i = 0; i <= 100; i++) {
-            if((i / 3) >= 1 && (i / 5) >= 1) {
+        int fizzCt = 0;
+        int buzzCt = 0;
+        
+        for(int i = 1; i <= 100; i++) {
+            fizzCt++;
+            buzzCt++;
+            
+            if (fizzCt == 3 && buzzCt == 5) {
                 System.out.println("FizzBuzz");
-            } else if((i / 3) >= 1) {
+                fizzCt = 0;
+                buzzCt = 0;
+            } else if(fizzCt == 3) {
                 System.out.println("Fizz");
-            } else if((i / 5) >= 1) {
+                fizzCt = 0;
+            } else if(buzzCt== 5) {
                 System.out.println("Buzz");
+                buzzCt = 0;
             } else {
                 System.out.println(i);
             }
